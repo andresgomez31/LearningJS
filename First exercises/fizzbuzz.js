d316a9afb3895770e48a0957ed25e8c40abf856b
andresgomez31 <gomez.moreno.andresaugusto.18@gmail.com>
2024-11-13 22:12:44 -0600

    Fixed logical problems and printing problems

--- First exercises/fizzbuzz.js
+++ First exercises/fizzbuzz.js
@@ -4,17 +4,21 @@ Para los múltiplos de 5, imprime Buzz en lugar del número.
 Para los múltiplos de 3 y 5, imprime FizzBuzz en lugar del número.
 */
 
-for(i = 0; i <= 100; i++){
-    if(i % 3 == 0){
-        console.log("Fizz");
+for(i = 1; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        process.stdout.write("FizzBuzz");
     }
-    if(i % 5 == 0){
-        console.log("Buzz");
+    else if(i % 3 === 0){
+        process.stdout.write("Fizz");
+    }
+    else if(i % 5 === 0){
+        process.stdout.write("Buzz");
     }
     else{
-        console.log(i);
+        process.stdout.write(i.toString());
     }
+
     if(i < 100){
-        console.log(", ");
+        process.stdout.write(", ");
     }
 }
