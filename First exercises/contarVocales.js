9f3aa6a2645a9b1c815ecafd519b0147ffc57513
andresgomez31 <gomez.moreno.andresaugusto.18@gmail.com>
2024-11-13 22:11:44 -0600

    Fixed

--- First exercises/contarVocales.js
+++ First exercises/contarVocales.js
@@ -2,7 +2,7 @@
 
 function contarVocales(s){
     contador = 0;
-    for(letter in s){
+    for(letter of s){
         if(letter.toLowerCase() === 'a' || letter.toLowerCase() === 'e' || letter.toLowerCase() === 'i' || letter.toLowerCase() === 'o' || letter.toLowerCase() === 'u'){
             contador++;
         }
